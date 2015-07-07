e9691fca53ea9590f01761e50be97830254d4a7c
Alexander Tinyaev <dix.alex@quorstudio.com>
2015-07-07 18:00:58 +0300

    Added jquery.cookie that was missed

--- assets/js/script.js
+++ assets/js/script.js
@@ -75,6 +75,7 @@ $(document).ready(function () {
     $('.start-screen').hide();
 
     userSignUp = true; // User already have account
+    $.cookie('user_sign_up', true);
 
     if ($(document).width() < 769) {
       hero_section.addClass('show-content');
@@ -102,6 +103,8 @@ $(document).ready(function () {
     $('.thank-you-screen').hide();
 
     userSignUp = true; // User signed up with whatcounts
+    $.cookie('user_sign_up', true);
+
     if ($(document).width() < 769) {
       hero_section.addClass('show-content');
       $('.week-modal-wrapper').show();
@@ -178,6 +181,13 @@ $(document).ready(function () {
 
     $(this).next().slideToggle();
   });
+
+  /**
+   * Check of user is already signed up and show interactive hero
+   */
+  if ($.cookie('user_sign_up') == 'true') {
+    $('.sign-in').click();
+  }
 
   /**
    * Initialize fancybox for videos
