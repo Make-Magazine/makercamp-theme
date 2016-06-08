efe48f0c07f22b1023760b782cf65c7db33d1b07
David Beauchamp <dbeauchamp@makermedia.com>
2016-06-08 15:43:03 -0700

    Adding a demo video modal

--- home.php
+++ home.php
@@ -4,7 +4,7 @@
     <div class="container text-center">
       <h2>WELCOME TO A</BR><strong>SUMMER OF MAKING</strong></h2>
       <div class="col-xs-12 col-sm-6 col-sm-offset-3 col-md-4 col-md-offset-4">
-        <h1>A Maker Camp is where kids ages 7-12 learn the magic of making by building cool stuff. Rady to help kids fire up their imaginations and challenge their skills? We're here to help you organize your own Maker Camp in your community or at home.</h1>
+        <h1>A Maker Camp is where kids ages 7-12 learn the magic of making by building cool stuff. Ready to help kids fire up their imaginations and challenge their skills? We're here to help you organize your own Maker Camp in your community or at home.</h1>
       </div>
     </div>
     <i class="fa fa-chevron-down" aria-hidden="true"></i>
@@ -57,13 +57,51 @@
     <dic class="clearfix"></div>
   </section>
 
+  <!-- FANCYBOX VIDEO STUFF -->
+  <div class="promo-video-outer" style="display: none;">
+    <div id="promo-video">
+      <video id="promo-video-c" controls preload="auto" poster="//staging.makercamp.com/wp-content/uploads/2016/06/promo-still.jpg" width="auto" height="auto" style="width:100% !important; height:auto !important">
+        <!--source src="<?php echo get_stylesheet_directory_uri(); ?>/video/promo-vo-50.mp4" type='video/mp4' /-->
+        <source src="//staging.makercamp.com/wp-content/uploads/2016/06/promo-vo-50.webm" type='video/webm' />
+        <!--source src="<?php echo get_stylesheet_directory_uri(); ?>/video/promo-vo-50.ogv" type='video/ogg' /-->
+        <p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web browser that supports HTML5 video</p>
+      </video>
+      <script>
+      jQuery( document ).ready(function() {
+        jQuery(".fancybox-promo").fancybox({
+          'beforeShow': function(){
+            jQuery(window).on({
+              'resize.fancybox' : function(){
+                jQuery.fancybox.update();
+              }
+            });
+            jQuery('#promo-video-c').get(0).play()
+          },
+          'afterClose': function(){
+            jQuery(window).off('resize.fancybox');
+          },
+          width      : '640',
+          height     : '360',
+          fitToView  : true,
+          closeClick : false,
+          openEffect : 'none',
+          closeEffect: 'none',
+          closeBtn   : 'true',
+          scrolling  : 'no'
+        });
+      });
+      </script>
+    </div>
+  </div>
+  <!-- END FANCYBOX VIDEO STUFF -->
+
   <section class="a-day-at-mc">
-    <img src="<?php echo get_template_directory_uri() . '/assets/img/hm_pg_video_bg.jpg' ?>" alt="Click here to see what its like to experience Maker Camp for kids" class="img-responsive" />
+    <img src="<?php echo get_template_directory_uri() . '/assets/img/hm_pg_video_bg.jpg' ?>" alt="Click here to see what its like to experience Maker Camp for kids" />
     <div class="container text-center">
       <h4>WATCH</h4>
       <h2>A DAY AT MAKER CAMP</h2>
       <h4>Ready, Set, Start Making!</h4>
-      <a class="blue-play-btn" href="#">
+      <a class="blue-play-btn fancybox-promo" href="#promo-video">
         <span class="fa-stack fa-2x">
           <i class="fa fa-circle fa-stack-2x"></i>
           <i class="fa fa-play fa-stack-1x fa-inverse"></i>
@@ -77,7 +115,7 @@
     <div class="container">
       <div class="row">
         <div class="col-xs-12 col-sm-6">
-          <p class="pull-right">THIS YEARS'S MAKER CAMP IS MADE</br> POSSIBLE BY THE GENEROUS SUPPORT OF:</P>
+          <p class="pull-right">THIS YEAR'S MAKER CAMP IS MADE</br> POSSIBLE BY THE GENEROUS SUPPORT OF:</P>
         </div>
         <div class="col-xs-12 col-sm-6">
           <img src="<?php echo get_template_directory_uri() . '/assets/img/att_globe_rgb.png' ?>" />
