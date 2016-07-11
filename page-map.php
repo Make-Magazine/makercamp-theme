56d3dfd2622a99aee62dbaf54bdec8c3c43aef05
David Beauchamp <dbeauchamp@makermedia.com>
2016-07-11 14:53:59 -0700

    Fixing bad formatted URL's on the explore page.

--- page-map.php
+++ page-map.php
@@ -285,12 +285,12 @@ usort( $addresses, function ( $a, $b ) {
 
 	<!-- Quick fix for URLs that are missing the http and become 404s -->
 	<script>
-	// 	$(document).ready(function () {
-	// 		$('a:not([href^="http://"]):not([href^="https://"])').each(function () {
-	// 			$(this).attr('href', 'http://' + $(this).attr('href'));
-	// 		})
-	// 	})
-	// </script>
+		$(document).ready(function () {
+			$('a:not([href^="http://"]):not([href^="https://"])').each(function () {
+				$(this).attr('href', 'http://' + $(this).attr('href'));
+			})
+		})
+	</script>
 	<!-- End Quick fix for URLs that are missing the http and become 404s -->
 
 <?php get_footer(); ?>
