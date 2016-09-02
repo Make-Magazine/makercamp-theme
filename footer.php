b382bb76d5109ef09eb4070124afdd8e1f49d2f8
Bill Olson <bolson@makermedia.com>
2016-09-01 18:07:10 -0700

    Privacy and terms for footer production

--- footer.php
+++ footer.php
@@ -185,7 +185,7 @@
 	</div>
 	<!-- END container -->
     <div class="copyright">
-      <p>Make: is a registered trademark and the Maker Camp logo and the Makey robot are trademarks of Maker Media.</p>
+      <p>Make: is a registered trademark and the Maker Camp logo and the Makey robot are trademarks of Maker Media | <a href="//makermedia.com/privacy/">Privacy</a> | <a href="//makermedia.com/terms/">Terms</a></p>
       <p>Copyright © 2004-2016 Maker Media, Inc. All rights reserved</p>
     </div>
 </footer>
